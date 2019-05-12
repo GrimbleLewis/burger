0ff1610d5325e64defdd9b4e1f488ef57457786c
Grimble <grimblelewisthethird@gmail.com>
2019-05-12 17:24:34 -0400

    created the database for the application in mysql and filled it with a few entries

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Hit Me With Your Best Shallot Burger');
+INSERT INTO burgers (burger_name) VALUES ('Papaya Was a Rolling Stone Burger');
+INSERT INTO burgers (burger_name) VALUES ('We’re Here, We’re Gruyere, Get Used to It Burger');
+INSERT INTO burgers (burger_name) VALUES ('I Know Why the Cajun Burger Sings');

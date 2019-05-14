fa4f5301735171ec860b3865962c507e22c8a435
Grimble <grimblelewisthethird@gmail.com>
2019-05-13 21:18:43 -0400

    added the DEVOUR ME button that changes the boolean value of devoured in the database and moves the burger to the right side of the screen. Added a burger-block.handlebars and moved the devour me button into there

--- config/orm.js
+++ config/orm.js
@@ -39,7 +39,7 @@ function printQuestionMarks(num) {
   }
 
   var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -48,7 +48,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    create: function(table, cols, vals, cb) {
+    insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -61,6 +61,23 @@ function printQuestionMarks(num) {
       console.log(queryString);
   
       connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
         if (err) {
           throw err;
         }
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    burger.all(function(data){
+    burger.selectAll(function(data){
         var hbsObject = {
             burgers: data
         };
@@ -15,7 +15,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create([
+    burger.insertOne([
         "burger_name"
     ], [
         req.body.name
@@ -23,5 +23,22 @@ router.post("/api/burgers", function(req, res) {
         res.json({ id: result.insertId });
     });
 });
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 module.exports = router;
